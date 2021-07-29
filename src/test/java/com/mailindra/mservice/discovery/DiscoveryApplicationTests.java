72f18e63389071e3010bca183b29e3f4b84d4937
Ronny Mailindra <ronny@g2academy.co>
2021-07-29 20:09:17 +0700

    initial working eureka server

---
+++ src/test/java/com/mailindra/mservice/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailindra.mservice.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
